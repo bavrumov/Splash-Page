3d0a6748d50b4b3de6450078318358d2022734c3
Boris Avrumov <bavrumov@hotmail.com>
2019-06-14 01:28:02 -0400

    Tachyon social media buttons

--- src/App.js
+++ src/App.js
@@ -1,46 +1,42 @@
 import React, { Component } from 'react';
-//import { BrowserRouter, Route, Link } from "react-router-dom";
-import {Header} from './Header.js';
-import Portrait from './Portrait.js';
+import {NavigationBar} from './NavigationBar.js';
+//import Portrait from './Portrait.js';
+import sunsetpic from './res/slide1.png';
 import './App.css';
+import Tachyonsocialfooter from './Tachyonsocialfooter.js'
 
 class App extends Component {
   render() {
     let themes = ["green-and-blue", "poncho", "pizelex"];
     let theme = themes[parseInt(Math.random()*3)];
-    //console.log(parseInt(Math.random()*3));
     //let theme = "green-and-blue";
     //let theme = "poncho";
     //let theme = "pizelex";
     return (
-      <div className="App">
-        
-        <header className={"App-header "+theme}>
-          <Header id="navigation-bar" names={["About Me", "Projects", "Contact"]}></Header>
+      <div className={"App " + theme}>
+        <NavigationBar></NavigationBar>
+        <header className={"App-header"}>
+          {/* <Header id="navigation-bar" names={["About Me", "Projects", "Contact"]}></Header>
           <div id="0" className="image-container"><Portrait></Portrait></div>
-          <div id="1" className="image-container"><Portrait name="ivy.jpg"></Portrait></div>
-
-          <p>
-            Anything we want can go here. Some sample text.
-          </p>
-            
+          <div id="1" className="image-container"><Portrait name="ivy.jpg"></Portrait></div> */}
           
-
-          <a
-            className="App-link"
-            href="http://bavrumov.github.io/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            My personal site
+          <a target="_blank" rel="noopener noreferrer" className="App-link" href="http://bavrumov.github.io/">
+            My old personal site
           </a>
-
         </header>
-        <div id="bottom"><Portrait></Portrait></div>
+          
+        <div className="App-body">
+          <img className="image-container" alt="a sunset" src={sunsetpic}/>
+          <div>Anything we want can go here. Some sample text.</div>
+        </div>
+
+        <div id="bottom"> <hr/>
+          <Tachyonsocialfooter></Tachyonsocialfooter>
+        </div>
+
       </div>
     );
   }
 }
-
 
 export default App;
