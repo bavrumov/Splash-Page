a04cd23c2b8db7d1bbcef5524b2f83aa38093ef5
Boris Avrumov <bavrumov@hotmail.com>
2019-06-15 01:45:49 -0400

    Dynamic Cards based on my work exp stored as json

--- src/SampleCard.js
+++ src/SampleCard.js
@@ -2,18 +2,18 @@ import React, { Component } from 'react';
 import { Card, CardImg, CardText, CardBody,
     CardTitle, CardSubtitle, CardHeader, CardFooter } from 'reactstrap';
 import BLK from './logos/BLK.jpg';
-
+// Used as a template and not actually needed in production
 export default class SampleCard extends Component {
   render() {
     return (
         <div>
         <Card>
             <CardHeader>July 2019 - </CardHeader>
-            <CardImg top width="100%" src={BLK} alt="BlackRock" />
+            <CardImg top width="50%" src={BLK} alt="BlackRock" />
             <CardBody>
-            <CardTitle></CardTitle>
-            <CardSubtitle></CardSubtitle>
-            <CardText></CardText>
+            <CardTitle>BlackRock</CardTitle>
+            <CardSubtitle>Subtitle Text</CardSubtitle>
+            <CardText>Card Text</CardText>
             <CardFooter>The world's largest asset manager</CardFooter>
             </CardBody>
         </Card>
