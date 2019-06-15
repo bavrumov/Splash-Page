89d52830b10a974986638cde61f28329f4d543b5
Boris Avrumov <bavrumov@hotmail.com>
2019-06-15 01:47:19 -0400

    Implement fixed navbar, work cards, &top pad class

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import {NavigationBar} from './NavigationBar.js';
+import Anchor from './Anchor.js';
 //import Portrait from './Portrait.js';
 import sunsetpic from './res/slide1.png';
 import './App.css';
 import Tachyonsocialfooter from './Tachyonsocialfooter.js'
 import {Container, Row, Col} from 'reactstrap';
-import SampleCard from './SampleCard.js';
+import WorkCards from './WorkCards.js';
 
 class App extends Component {
   render() {
@@ -22,25 +23,25 @@ class App extends Component {
           <div id="0" className="image-container"><Portrait></Portrait></div>
           <div id="1" className="image-container"><Portrait name="ivy.jpg"></Portrait></div> */}
           
-          <a target="_blank" rel="noopener noreferrer" className="App-link" href="http://bavrumov.github.io/">
-            My old personal site
-          </a>
+          <Anchor name="My old personal site" url="http://bavrumov.github.io/"></Anchor>
         </header>
           
         <div className="App-body">
-          <Container>
-          {/* <Row sm={{size:10, offset:1}}> */}
-          <Row className="job-row">
-            <Col md={4} xs={6}>
-              <SampleCard></SampleCard>
-            </Col>
-            <Col md={4}>
-              <img className="image-container" alt="a sunset" src={sunsetpic}/>
-            </Col>
-            <Col md={4}>
-            <div>Anything we want can go here. Some sample text.</div>
-            </Col>
-          </Row>
+
+          <Container id="App-body">
+            <Row className="padded-top">
+              <Col md={{size:8, offset:2}}>
+                <img className="image-container" alt="a sunset" src={sunsetpic}/>
+              </Col>
+            </Row>
+          </Container>
+
+          <Container id="Jobs">
+            <Row className="padded-top">
+              <Col>
+                <WorkCards></WorkCards>
+              </Col>
+            </Row>
           </Container>
           
         </div>
