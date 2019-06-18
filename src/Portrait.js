9b162cd61c9377ebd89ea7e2be5ccc7b475b660b
Boris Avrumov <bavrumov@hotmail.com>
2019-06-18 17:43:49 -0400

    Reinstating protraits

--- src/Portrait.js
+++ src/Portrait.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react'
 import me from './assets/me.jpg'
-import ivy from './assets/ivy.jpg'
 
 class Portrait extends Component {
     render() {
-        //alert();
         return (
-            <img className="rounded-image"
-                src={(this.props.name==="ivy.jpg") ? ivy : me}
+            <img 
+                style={{borderRadius:"8%"}}
+                src={me}
                 width="100%"
                 alt="It me"
             />
