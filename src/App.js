b0d1a1cd69f55f1a0f5cd0f229f9e9e0f4db5129
Boris Avrumov <bavrumov@hotmail.com>
2019-06-18 19:21:33 -0400

    Cleaner main page

--- src/App.js
+++ src/App.js
@@ -1,46 +1,35 @@
 import React, { Component } from 'react';
-import {NavigationBar} from './NavigationBar.js';
-import Anchor from './Anchor.js';
-//import Portrait from './Portrait.js';
-import sunsetpic from './res/slide1.png';
+import { NavigationBar } from './NavigationBar.js';
+import Blurb from './Blurb.js';
+import classNames from 'classnames';
 import './App.css';
 import Tachyonsocialfooter from './Tachyonsocialfooter.js'
-import {Container, Row, Col} from 'reactstrap';
+import { Container, Row, Col } from 'reactstrap';
 import WorkCards from './WorkCards.js';
 import Carousel from './Carousel.js';
 
 class App extends Component {
   render() {
-    //let themes = ["green-and-blue", "poncho", "pizelex"];
     //let theme = themes[parseInt(Math.random()*3)];
     let theme = "green-and-blue";
     //let theme = "poncho";
     //let theme = "pizelex";
+
     return (
-      <div className={"App " + theme}>
-        <NavigationBar></NavigationBar>
-        <header className={"App-header"}>
-          {/* <Header id="navigation-bar" names={["About Me", "Projects", "Contact"]}></Header>
-          <div id="0" className="image-container"><Portrait></Portrait></div>
-          <div id="1" className="image-container"><Portrait name="ivy.jpg"></Portrait></div> */}
-          
-          <Anchor name="My old personal site" url="http://bavrumov.github.io/"></Anchor>
-        </header>
+      <div className={classNames("App", theme)}>
+        <NavigationBar />
           
         <div className="App-body">
 
           <Container id="App-body">
             <Row className="padded-top">
-              
-              <Col md={{size:8, offset:2}}>
-                <img className="image-container" alt="a sunset" src={sunsetpic}/>
-              </Col>
+              More Content Here
             </Row>
           </Container>
 
-          <Container>
+          <Container id="Education">
             <Row className="padded-top">
-              <Col xs={12}><Carousel></Carousel></Col>
+              <Carousel></Carousel>
             </Row>
           </Container>
 
@@ -53,7 +42,7 @@ class App extends Component {
           </Container>
           
         </div>
-
+        <Blurb />
         <div id="bottom"> <hr/>
           <Tachyonsocialfooter></Tachyonsocialfooter>
         </div>
