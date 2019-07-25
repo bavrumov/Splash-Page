c2e776de9dc7059f268429a29eb95f5ea9301f86
Boris Avrumov <bavrumov@hotmail.com>
2019-07-25 16:19:11 -0400

    Addition of Animate On Scroll and sliding headers

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ import Tachyonsocialfooter from './Tachyonsocialfooter.js'
 import { Container, Row, Col } from 'reactstrap';
 import WorkCards from './WorkCards.js';
 import Carousel from './Carousel.js';
+//Animation library import and CSS
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 class App extends Component {
   constructor(props) {
@@ -17,6 +20,12 @@ class App extends Component {
       themes: ["green-and-blue", "poncho", "pizelex"],
       theme: "green-and-blue"
     };
+  }
+  componentDidMount(){
+    AOS.init({
+      duration : 1800,
+      once: true,
+    })
   }
 
   handleTheme(x) {
@@ -35,16 +44,20 @@ class App extends Component {
 
           <Container id="App-body">
             <Row className="padded-top">
-              More Content Here
+              {/* More Content Here */}
             </Row>
           </Container>
 
+          <header data-aos="fade-left" className={classNames("App-header", "slide-header", "right")}>Education</header>
           <Container id="Education">
             <Row className="padded-top">
               <Carousel></Carousel>
             </Row>
           </Container>
+          <br /><br />
 
+
+          <header data-aos="fade-right" className={classNames("App-header", "slide-header", "left")}>Work Experience</header><br />
           <Container id="Jobs">
             <Row className="padded-top">
               <Col>
@@ -52,7 +65,15 @@ class App extends Component {
               </Col>
             </Row>
           </Container>
-          
+          <br /><br />
+
+          <header data-aos="fade-left" className={classNames("App-header", "slide-header", "right")}>Hobbies</header>
+          <Container id="Hobbies">
+            <Row className="padded-top">
+              {/* More Content Here */}
+            </Row>
+          </Container>
+
         </div>
         <Blurb />
         <div id="bottom"> <hr/>
